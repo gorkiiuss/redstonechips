c363e624bfbc28701baec15dc349c457ebee6788
Gorka Puente Díez <gorkapuentediez@gmail.com>
2024-06-09 20:53:35 +0200

    Initial commit

---
+++ src/client/java/com/github/gorkiiuss/redstonechips/RedstoneChipsClient.java
@@ -0,0 +1,10 @@
+package com.github.gorkiiuss.redstonechips;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RedstoneChipsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
